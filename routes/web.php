c84c4afa8ac5fcd82433d2192855707c83bf0be4
Juan Miguel Recondo <juanmiguel.recondo@gmail.com>
2025-08-24 22:52:56 +0800

    feat: Add routes to the main pages
    The dashboard becomes the root page.

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,34 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return Inertia::render('dashboard');
+})->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+//Route::middleware(['auth', 'verified'])->group(function () {
+//    Route::get('dashboard', function () {
+//        return Inertia::render('dashboard');
+//    })->name('dashboard');
+//});
+
+Route::get('about', function () {
+    return Inertia::render('about');
+})->name('about');
+
+Route::get('education', function () {
+    return Inertia::render('education');
+})->name('education');
+
+Route::get('skills', function () {
+    return Inertia::render('skills');
+})->name('skills');
+
+Route::get('portfolio', function () {
+    return Inertia::render('portfolio');
+})->name('portfolio');
+
+Route::get('blog', function () {
+    return Inertia::render('blog');
+})->name('blog');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
